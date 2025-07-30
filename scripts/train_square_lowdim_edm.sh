3a66e389d2d4ff5a426d9e5a7f2223b19e2b1c8e
cspark <matthewcool@snu.ac.kr>
2025-07-30 09:17:17 +0900

    script changed for simple work

--- scripts/train_square_lowdim_edm.sh
+++ scripts/train_square_lowdim_edm.sh
@@ -5,8 +5,17 @@ logging.group=square_lowdim_reproduction \
 training.device=cuda:0
 
 # Check if there is debug mode...
-
+task_config_name=square
+debug=true
+device=cuda:0
+if [ "$debug" = true ]; then
+    subdir="debug/"
+else
+    subdir="/"
+fi
+run_dir='outputs/${task.name}_${task.dataset_type}_reproduction/train_by_seed_edm/'$subdir'seed_${training.seed}_${now:%Y.%m.%d-%H.%M.%S}_${name}_${task_name}_cnn_${horizon}'
+echo $run_dir
 for seed in 0; do
-    echo -e "\033[32m[Training edm_lift_lowdim with seed: ${seed}]\033[0m"
-    python train.py  --config-dir=configs/ --config-name=edm_tool_hang_lowdim.yaml hydra.run.dir='outputs/${task.name}_${task.dataset_type}_reproduction/train_by_seed_edm/seed_${training.seed}_${now:%Y.%m.%d-%H.%M.%S}_${name}_${task_name}_cnn_${horizon}' logging.group='${task.name}_${task.dataset_type}_edm' logging.name='seed_${training.seed}_edm_${now:%Y.%m.%d-%H.%M.%S}_${name}_${task_name}_${horizon}'  horizon=32 task.dataset.horizon=32 task.dataset.pad_after=31 training.seed=$seed training.device=cuda:0 obs_as_global_cond=True training.num_epochs=2000 policy.horizon=32 training.debug=true
+    echo -e "\033[32m[Training ${task_config_name} with seed: ${seed}]\033[0m"
+    python train.py  --config-dir=configs/ --config-name=edm_${task_config_name}_lowdim.yaml hydra.run.dir=$run_dir logging.group='${task.name}_${task.dataset_type}_edm' logging.name='seed_${training.seed}_edm_${now:%Y.%m.%d-%H.%M.%S}_${name}_${task_name}_${horizon}'  horizon=32 task.dataset.horizon=32 task.dataset.pad_after=31 training.seed=$seed training.device=$device obs_as_global_cond=True training.num_epochs=2000 policy.horizon=32 training.debug=$debug
 done
